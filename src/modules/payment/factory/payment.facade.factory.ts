53d4eb439fe8607f3a123c57c050f458044d6078
Flávio Dutra <flaviomdutra@outlook.com>
2024-02-10 18:38:35 -0300

    Add PaymentFacadeInterface import to PaymentFacadeFactory

--- src/modules/payment/factory/payment.facade.factory.ts
+++ src/modules/payment/factory/payment.facade.factory.ts
@@ -1,9 +1,10 @@
+import { PaymentFacadeInterface } from '../facade/facade.interface'
 import { PaymentFacade } from '../facade/payment.facade'
 import { TransactionRepository } from '../repository/transaction.repository'
 import { ProcessPaymentUseCase } from '../use-case/process-payment/process-payment.usecase'
 
 export class PaymentFacadeFactory {
-  static create() {
+  static create(): PaymentFacadeInterface {
     const transactionRepository = new TransactionRepository()
     const processPaymentUseCase = new ProcessPaymentUseCase(
       transactionRepository,
