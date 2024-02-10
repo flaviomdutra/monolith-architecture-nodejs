9992c26644dbadc234f227c4bde861ecb18c33dc
Flávio Dutra <flaviomdutra@outlook.com>
2024-02-09 22:37:18 -0300

    Add client Use Case

---
+++ src/modules/client-adm/use-case/add-client/add-client.usecase.spec.ts
@@ -0,0 +1,28 @@
+import { AddClientUsecase } from './add-client.usecase'
+
+const MockRepository = jest.fn(() => ({
+  add: jest.fn(),
+  find: jest.fn(),
+}))
+
+describe('Add Client Usecase unit test', () => {
+  it('should add a client', async () => {
+    const repository = MockRepository()
+    const usecase = new AddClientUsecase(repository)
+
+    const input = {
+      id: '123',
+      name: 'John Doe',
+      email: 'jonh.doe@email.com',
+      address: '1234 Elm Street',
+    }
+
+    const output = await usecase.execute(input)
+
+    expect(repository.add).toHaveBeenCalled()
+    expect(output.id).toBe(input.id)
+    expect(output.name).toBe(input.name)
+    expect(output.email).toBe(input.email)
+    expect(output.address).toBe(input.address)
+  })
+})
