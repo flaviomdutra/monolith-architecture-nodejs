6e071f9dc14d2887fa745265b987cd41d3e0c073
Flávio Dutra <flaviomdutra@outlook.com>
2024-02-09 23:07:47 -0300

    Add client creation test to ClientRepository

--- src/modules/client-adm/repository/client.repository.spec.ts
+++ src/modules/client-adm/repository/client.repository.spec.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from 'sequelize-typescript'
 
+import { Id } from '../../@shared/domain/value-object/id.value-object'
+import { Client } from '../domain/client.entity'
 import { ClientModel } from './client.model'
 import { ClientRepository } from './client.repository'
 
@@ -22,6 +24,28 @@ describe('ClientRepository test', () => {
     await sequelize.close()
   })
 
+  it('should create a client', async () => {
+    const client = new Client({
+      id: new Id('1'),
+      name: 'John Doe',
+      email: 'jonh.doe@email.com',
+      address: '1234 Elm Street',
+    })
+
+    const repository = new ClientRepository()
+    await repository.add(client)
+
+    const clientDB = await ClientModel.findOne({ where: { id: client.id.id } })
+
+    expect(clientDB.id).toEqual(client.id.id)
+    expect(clientDB.name).toEqual(client.name)
+    expect(clientDB.email).toEqual(client.email)
+    expect(clientDB.address).toEqual(client.address)
+
+    expect(clientDB.createdAt).toEqual(client.createdAt)
+    expect(clientDB.updatedAt).toEqual(client.updatedAt)
+  })
+
   it('should find a client', async () => {
     const client = await ClientModel.create({
       id: '1',
@@ -34,7 +58,7 @@ describe('ClientRepository test', () => {
 
     const repository = new ClientRepository()
     const result = await repository.find(client.id)
-    console.log('result', result)
+
     expect(result.id.id).toEqual(client.id)
     expect(result.name).toEqual(client.name)
     expect(result.email).toEqual(client.email)
